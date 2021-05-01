ebba82f469f35961158bd61270e929dbb5452f87
totobono4 <totobono4@outlook.fr>
2021-05-01 12:24:00 +0200

    Working version without api implementation

--- modules/minecraft/minecraft.js
+++ modules/minecraft/minecraft.js
@@ -1,46 +1,98 @@
 const Discord = require("discord.js");
 const { spawn } = require("child_process");
-const bat = require.resolve("./servers/ProjectOzone3/start.bat");
+const Path = require("path");
+const FileSystem = require('fs');
 
 const { client } = require("../../main.js");
 const { Console } = require("console");
 const { finished } = require("stream");
 
-const channelIdServer = "828559514495090688";
+const serverFileName = Path.resolve(__dirname, "servers.json");
+const serverJson = require(serverFileName);
+const { cwd } = require("process");
+
+const channelIdServer = "797554643018055720";
+//const channelIdServer = "828559514495090688";
 const channelIdChat = "828571744670973962";
 
-const regexServer = /(.*\[Server thread\/INFO\] \[minecraft\/DedicatedServer\]: [^\/].*|.*\[Server thread\/INFO\] \[minecraft\/MinecraftServer\]:.*)/;
-const regexChatMinecraft = /\[Server thread\/INFO\] \[minecraft\/DedicatedServer\]: <(.*)> ([^\/].*)/;
-const regexChatDiscord = /(^[^\/].*)/;
-const regexOnline = /(.*\[Server thread\/INFO\] \[minecraft\/DedicatedServer\]: Done)/;
+let serverPath = null;
+let bat = null;
+
+let regexServer = null;
+let regexChatMinecraft = null;
+let regexChatDiscord = /(^[^\/].*)/;
+let regexOnline = null;
 
 var server = null;
 var serverStatut = "offline";
+const statut = {
+    starting : "starting",
+    online   : "online",
+    shutting : "shutting",
+    offline  : "offline"
+}
 
 exports.whitelist = [
     "232047122868994049",
     "427251511500996608"
 ]
 
-exports.commands = [
-    "minecraft statut",
-    "minecraft start",
-    "minecraft stop"
-]
+exports.commands = {
+    test:     "minecraft test",
+
+    versions: "minecraft versions",
+
+    list:     "minecraft list",
+    select:   "minecraft select",
+
+    statut:   "minecraft statut",
+    start:    "minecraft start",
+    stop:     "minecraft stop"
+}
 
 exports.minecraftModule = async (prefix, message) => {
 
-    if (message.content.startsWith(`${prefix}minecraft statut`)) {
+    if (message.content.startsWith(`${prefix}${this.commands.versions}`)) {
+		listVersions();
+		return;
+	}
+
+    if (message.content.startsWith(`${prefix}${this.commands.list}`)) {
+		listServers();
+		return;
+	}
+
+    if (message.content.startsWith(`${prefix}${this.commands.select}`)) {
+		selectServer(message.content.split(`${prefix}${this.commands.select} `).pop());
+		return;
+	}
+
+    if (serverJson.selected == null)
+    {
+        const ChannelServer = client.channels.cache.get(channelIdServer)
+        ChannelServer.send('aucun serveur n\'est sélectionné:');
+        listServers();
+        return;
+    }
+
+    serverPath = Path.resolve(__dirname , serverJson.servers[serverJson.selected].path);
+    bat = Path.resolve(serverPath, 'start.bat');
+
+    regexServer        = new RegExp(serverJson.servers[serverJson.selected].regex.server);
+    regexChatMinecraft = new RegExp(serverJson.servers[serverJson.selected].regex.chatMinecraft);
+    regexOnline        = new RegExp(serverJson.servers[serverJson.selected].regex.online);
+
+    if (message.content.startsWith(`${prefix}${this.commands.statut}`)) {
 		statutServer();
 		return;
 	}
 
-    if (message.content.startsWith(`${prefix}minecraft start`)) {
+    if (message.content.startsWith(`${prefix}${this.commands.start}`)) {
 		startServer();
 		return;
 	}
 
-    if (message.content.startsWith(`${prefix}minecraft stop`)) {
+    if (message.content.startsWith(`${prefix}${this.commands.stop}`)) {
 		stopServer();
 		return;
 	}
@@ -63,6 +115,36 @@ client.on("message", async message => {
     }
 });
 
+async function listVersions()
+{
+    const ChannelServer = client.channels.cache.get(channelIdServer)
+}
+
+async function listServers()
+{
+    const ChannelServer = client.channels.cache.get(channelIdServer)
+
+    ChannelServer.send(Object.keys(serverJson.servers));
+}
+
+async function selectServer(selectedServer)
+{
+    const ChannelServer = client.channels.cache.get(channelIdServer)
+
+    if (serverJson.servers[selectedServer] == undefined)
+    {
+        ChannelServer.send(['le serveur', selectedServer, 'n\'existe pas.'].join(" "));
+        return;
+    }
+
+    serverJson.selected = selectedServer;
+
+    FileSystem.writeFile(serverFileName, JSON.stringify(serverJson, null, 2), function writeJSON(err) {
+        if (err) return console.log(err);
+        ChannelServer.send(['Serveur', selectedServer, 'sélectionné avec succès.'].join(" "));
+    });
+}
+
 async function statutServer()
 {
     const ChannelServer = client.channels.cache.get(channelIdServer);
@@ -75,19 +157,21 @@ async function startServer()
     const ChannelServer = client.channels.cache.get(channelIdServer)
     const ChannelChat = client.channels.cache.get(channelIdChat)
 
-    if (serverStatut != "offline")
+    if (serverStatut != statut.offline)
     {
         ChannelServer.send("minecraft server already started...");
         return;
     }
 
-    server = spawn(bat);
+    server = spawn(bat, { cwd: serverPath });
 
     server.stdout.on('data', (data) => {
+
         if (regexServer.test(data)) ChannelServer.send("" + data);
         if (regexOnline.test(data))
         {
-            serverStatut = "online";
+            serverStatut = statut.online;
+            logMessage = null;
             statutServer();
         }
 
@@ -101,16 +185,18 @@ async function startServer()
     })
 
     server.stderr.on('data', function (data) {
+        console.log("" + data);
         if (regexServer.test(data)) ChannelServer.send("" + data);
     });
     
     server.on('exit', function (code) {
-        serverStatut = "offline";
+        serverStatut = statut.offline;
+        logMessage = null;
         statutServer();
     });
 
     serverStarted = true;
-    serverStatut = "starting";
+    serverStatut = statut.starting;
     statutServer();
 }
 
@@ -118,7 +204,7 @@ async function stopServer()
 {
     const ChannelServer = client.channels.cache.get(channelIdServer)
 
-    if (serverStatut != "online") 
+    if (serverStatut != statut.online) 
     {
         ChannelServer.send("Cannot stop a dead server.");
         return;
@@ -126,6 +212,6 @@ async function stopServer()
 
     server.stdin.write("stop\n");
     server.stdin.end();
-    serverStatut = "shutting"
+    serverStatut = statut.shutting;
     statutServer();
 }
---
+++ modules/minecraft/template/start.bat
@@ -0,0 +1,3 @@
+color e
+java -Xmx6144M -Xms1024M -jar server.jar nogui 
+pause
